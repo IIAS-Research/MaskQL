55b53ebc15ece7a72717d2a4a46dbae3da6200d1
Rudy Merieux <rmerieux@chu-reims.fr>
2025-08-19 01:03:13 +0200

    feat: add rules in seed

--- maskql/alembic/seeds.py
+++ maskql/alembic/seeds.py
@@ -9,7 +9,16 @@ def seed_test_data(conn):
     """))
     conn.execute(text("""
         INSERT INTO users(username, password)
-        VALUES ('demo', '$argon2id$v=19$m=65536,t=3,p=4$aU0JYQxBqJUSYkxJiVHKuQ$ubzb5ljGTqoSXxwv58VV2lgM/D1Iu/koIlk29ngwMeg')
+        VALUES
+            ('demo', '$argon2id$v=19$m=65536,t=3,p=4$aU0JYQxBqJUSYkxJiVHKuQ$ubzb5ljGTqoSXxwv58VV2lgM/D1Iu/koIlk29ngwMeg'),
+            ('demo1', '$argon2id$v=19$m=65536,t=3,p=4$aU0JYQxBqJUSYkxJiVHKuQ$ubzb5ljGTqoSXxwv58VV2lgM/D1Iu/koIlk29ngwMeg')
+        ON CONFLICT (id) DO NOTHING
+    """))
+    conn.execute(text("""
+        INSERT INTO rules(path, allow, effect, catalog_id, user_id)
+        VALUES 
+            ('client.name', true, 'UPPER(name)', 1, 1),
+            ('client', true, 'name like \'Al%\'', 1, 1)
         ON CONFLICT (id) DO NOTHING
     """))
     
